2ff85fc8bc689095a769e0fb6706f0f7d771708f
Matias Verges <matismasters@gmail.com>
2023-12-08 14:06:44 -0300

    Gem initial commit

---
+++ lib/easy_to_dictate_numbers.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "easy_to_dictate_numbers/version"
+
+module EasyToDictateNumbers
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/easy_to_dictate_numbers_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EasyToDictateNumbers do
+  it "has a version number" do
+    expect(EasyToDictateNumbers::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
