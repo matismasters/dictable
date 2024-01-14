8ff623b74c5ca14c027c8a2f0b8ebf4f72132dd4
Matias Verges <matismasters@gmail.com>
2024-01-14 17:26:21 -0300

    Move extend primitives require to lib
    Leasson learned: When setting the `path:` parameter to a gem
    in the Gemfile, the `.gemspec` file is executed as part of loading
    the gem. If no path is set, bundler does not execute the `.gemspec`
    file, because it already pulled all the information it needed from
    it at that point to create the Gemfile.lock and manage the
    dependencies.
    
    As I had the require for the `extend_primitives.rb` on the `.gemspec`
    file, it only extended the primitives when using the `path:` parameter.
    
    That lead me to double check everything around the code pushed to
    RubyGems, my local code, the packaging of the gems, etc. An hour
    of uncertainty seeing that the code was exactly the same locally
    as the code coming from ruby gems.
    
    It is fixed now.
    Lesson Learned.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,7 @@
 ## [0.1.1] - 2024-01-14
 
 - Change documentation
+
+## [0.1.2] - 2024-01-14
+
+- Moved extend primitives require to lib/easy_to_dictate_numbers.rb.
--- lib/easy_to_dictate_numbers.rb
+++ lib/easy_to_dictate_numbers.rb
@@ -2,6 +2,7 @@
 
 # lib/easy_to_dictate_numbers.rb
 require_relative 'easy_to_dictate_numbers/version'
+require_relative 'easy_to_dictate_numbers/extend_primitives'
 
 module EasyToDictateNumbers
   class Error < StandardError; end
